0a4d1bedfdb6b2ba26549ca525c005d8cb941537
sekiya4019 <sekiya4019@gmail.com>
2018-07-09 07:37:54 +0900

    Link to Vendor_show from index

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -2,4 +2,9 @@ class VendorsController < ApplicationController
   def index
     @vendors = Vendor.all
   end
+
+  def show
+    @vendor = Vendor.find_by(id: params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   get 'top' => "home#top"
   get 'about' => "home#about"
 
+  get "/vendors/index" => "vendors#index"
+  # 極力最後にする必要のあるルーティング
+  get "/vendors/:id" => "vendors#show"
+
+
 end
