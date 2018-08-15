7a54dd62bde76f8efa392d73aec898999772626a
sekiya4019 <sekiya4019@gmail.com>
2018-08-16 07:41:00 +0900

    HighChart install

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,7 @@
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+
+//= require highcharts/highcharts
+//= require highcharts/highcharts-more
+//= require highcharts/highstock
--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -5,6 +5,14 @@ class VendorsController < ApplicationController
 
   def show
     @vendor = Vendor.find_by(id: params[:id])
+    category = [1,3,5,7]
+    current_quantity = [1000,5000,3000,8000]
+
+    @graph = LazyHighCharts::HighChart.new('graph') do |f|
+      f.title(text: 'ItemXXXの在庫の推移')
+      f.xAxis(categories: category)
+      f.series(name: '在庫数', data: current_quantity)
+    end
   end
 
 end
