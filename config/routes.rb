a87424029bbd960a296102c89303eddebe6d1035
sekiya4019 <sekiya4019@gmail.com>
2018-08-28 06:54:01 +0900

    Create Users_indexPage

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,18 @@ Rails.application.routes.draw do
   get 'terms' => "home#terms"
   get 'privacy' => "home#privacy"
 
+#----ベンダー情報------------------------
   get 'vendors/index' => "vendors#index"
   # 極力最後にする必要のあるルーティング
   get 'vendors/:id' => "vendors#show"
 
+#----システム情報------------------------
   get 'systems/index' => "systems#index"
   # 極力最後にする必要のあるルーティング
   get 'systems/:id' => "systems#show"
+
+#----ユーザー情報------------------------
+  get 'users/index' => "users#index"
 
 
 end
--- db/seeds_user.rb
+++
@@ -1,6 +0,0 @@
-# ユーザー情報を初期設定
-User.create(mail_adress: "sekiya@gmail.com", password: "password", nickname: "sekiya")
-User.create(mail_adress: "maekawa@gmail.com", password: "password", nickname: "maekawa")
-User.create(mail_adress: "tokuma@gmail.com", password: "password", nickname: "tokuma")
-User.create(mail_adress: "inagaki@gmail.com", password: "password", nickname: "inagaki")
-User.create(mail_adress: "wakita@gmail.com", password: "password", nickname: "wakita")
