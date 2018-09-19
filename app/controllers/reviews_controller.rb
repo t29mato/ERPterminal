d5bea43c4eb09af697d54baf65da10ff6f49aeb9
sekiya4019 <sekiya4019@gmail.com>
2018-09-20 07:37:43 +0900

    Design change review_indexPage complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,9 +2,6 @@ class ReviewsController < ApplicationController
 
   def index
     @reviews = Review.all
-    pp "*********************"
-    pp @reviews
-    pp "*********************"
   end
 
 end
