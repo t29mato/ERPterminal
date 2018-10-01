51322447bfba9214142a54ca5bb5df5569f00a4e
sekiya4019 <sekiya4019@gmail.com>
2018-10-02 07:11:37 +0900

    Add Review to get_category_name

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,11 @@
 class Review < ApplicationRecord
+
+  def get_category_name
+    return Category.find_by(id: self.category_id).name
+  end
+
+  def get_user
+    return User.find_by(id: self.user_id)
+  end
+
 end
