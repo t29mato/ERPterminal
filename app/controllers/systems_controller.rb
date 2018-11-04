5ddd22764f58e5fc1e0c2a5864effc9a42941f01
sekiya4019 <sekiya4019@gmail.com>
2018-11-04 10:16:17 +0900

    Design change System_show

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -5,6 +5,41 @@ class SystemsController < ApplicationController
 
   def show
     @system = System.find_by(id: params[:id])
+    @categories = Category.all
+    @vendor_systems = @system.get_vendor_systems
+    @reviews = @system.get_reviews(@vendor_systems)
+
+    @graph = LazyHighCharts::HighChart.new('graph') do |r|
+      r.pane(size: '80%')
+      r.xAxis(
+        categories: Category.pluck(:name),
+        tickmarkPlacement: 'on',
+        lineWidth: 0
+      )
+      r.yAxis(
+        gridLineInterpolation: 'polygon',
+        lineWidth: 0,
+        min: 0,
+        max: 5,
+        tickInterval: 1
+      )
+      r.tooltip(
+        shared: true,
+        pointFormat: '<span style="color:{series.color}">{series.name}: <b>${point.y:,.0f}</b><br/>'
+      )
+      r.legend(
+        y: 70,
+      )
+      r.series(
+        ame: 'Allocated Budget',
+        data: [3.5, 2.8, 3.2, 3.7, 3.5, 4.0, 3.6, 2.5],
+        pointPlacement: 'on'
+      )
+      r.chart(
+        polar: true,
+        type: 'area'
+      )
+    end
   end
 
 end
--- app/models/system.rb
+++ app/models/system.rb
@@ -3,4 +3,13 @@ class System < ApplicationRecord
   has_many :vendors, through: :vendor_systems
   has_many :vendor_systems
 
+  def get_vendor_systems
+    @vendor_systems = VendorSystem.where(system_id: self.id)
+    return @vendor_systems.sort_by{|vendor_system| vendor_system[:updated_at]}
+  end
+
+  def get_reviews(vendor_systems_hash)
+    @reviews = Review.where(vendor_system_id: vendor_systems_hash.pluck(:id))
+  end
+
 end
