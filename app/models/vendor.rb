559f66eb1452642e317f1c3c47e80d8f5b18ea8e
sekiya4019 <sekiya4019@gmail.com>
2018-11-04 23:11:19 +0900

    Add systems name to vendor_indexPage 70%

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -8,6 +8,10 @@ class Vendor < ApplicationRecord
     return @vendor_systems.sort_by{|vendor_system| vendor_system[:updated_at]}
   end
 
+  def get_systems(vendor_systems_hash)
+    @systems = System.where(id: vendor_systems_hash.pluck(:system_id))
+  end
+
   def get_reviews(vendor_systems_hash)
     @reviews = Review.where(vendor_system_id: vendor_systems_hash.pluck(:id))
   end
